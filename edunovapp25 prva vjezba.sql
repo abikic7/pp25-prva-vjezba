7fd175b575cf284adb661ba3e79a771fb24de168
abikic7 <104563758+abikic7@users.noreply.github.com>
2022-05-08 12:09:23 +0200

    pp25 vježba tablica

--- edunovapp25 prva vjezba.sql
+++ edunovapp25 prva vjezba.sql
@@ -1,46 +1,29 @@
 #   ovo je SQL skripta 
-#   naredba za izvođenje
-#   C:\xampp\mysql\bin\mysql -uroot < C:\Users\Kleep\Documents\GitHub\EdunovaPP25\SQL\edunovapp25.sql
+
+
 #   C:\xampp\mysql\bin\mysql -uroot < C:\Users\abikic\Documents\GitHub\EdunovaPP25\SQL\edunovapp25.sql
 
     drop database if exists edunovapp25;
     create database edunovapp25;
     use edunovapp25;
 
-    create table smjer (
-        naziv   varchar(50),
-        cijena   varchar(50),
-        trajanje varchar(50),
-        upisnina varchar(50);
-        certificiran varchar(50)
+    create table fzdjelatnica (
+        ime   varchar(50),
+        prezime   varchar(50),
+        OIB varchar(50)
+        
     );
 
- create table grupa(
-     naziv varchar(50),
-     datumpocetka varchar (50),
-     maksimalnopolaznika varchar(50),
-     smjer varchar(50),
-     predavac varchar(50)
+ create table fzkorisnik (
+     ime varchar(50),
+     prezime varchar(50),
+     adresa varchar (50)
+
  ) ;    
 
-     create table clan (
-    grupa varchar(50),
-    polaznik varchar(50)
+     create table usluga (
+    sisanje  varchar(50),
+    brijanje varchar(50),
+    bojanje varchar (50)
 );
 
-create table predavac (
-    osoba varchar(50),
-    IBAN varchar(50)
-);
-
-create table polaznik (
-    osoba varchar(50),
-    broj_ugovora varchar(50)
-);
-
-create table osoba (
-    ime varchar(50),
-    prezime varchar(50),
-    email varchar(50),
-    OIB varchar(50)
-);
